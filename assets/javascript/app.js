93f731c6424057e9a41d4419da4b1c87ba51fc0a
unknown <tberry6@gmail.com>
2018-03-22 10:01:34 -0700

    Added real questions and some styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,18 +19,24 @@ function Question(q, a, c, id) {
     };
 }
 
-var q1 = new Question("Answer is 1", ["1", "2", "3"], "1", "q1");
-var q2 = new Question("Answer is 2", ["1", "2", "3"], "2", "q2");
-var q3 = new Question("Answer is 3", ["1", "2", "3"], "3", "q3");
-var questions = [q1, q2, q3];
-var curr = 0;
-var correct = 0;
-var time = 5;
+var q1 = new Question("Which hobbit won the ring in a riddle game with Gollum?", ["Frodo Baggins", "Bilbo Baggins", "Samwise Gamgee", "Meriadoc Brandybuck"], "Bilbo Baggins", "q1");
+var q2 = new Question("In what age does the bulk of the action in The Lord of the Rings occur?", ["First Age", "Second Age", "Third Age", "Fourth Age"], "Third Age", "q2");
+var q3 = new Question("How many hobbits were part of the Fellowship of the Ring?", ["One", "Two", "Three", "Four"], "Four", "q3");
+var q4 = new Question("Which of the following is not a dwarf in Thorin Oakenshield's band?", ["Balin","Dwalin","Gimli","Gloin"], "Gimli", "q4");
+var questions = [q1, q2, q3, q4];
+var curr;
+var correct;
+var time;
 $("#time").html(time);
 var countdown;
 
 function start() {
+    correct = 0;
+    curr = 0;
+    time = 5;
+    $("#timer").html("Time Remaining: <span id=\"time\">" + time + "</span> Seconds");
     $("#questions").html(questions[curr].display());
+    $("#results").empty();
     countdown = setTimeout(check, 5000);
 
 }
@@ -44,7 +50,9 @@ function nextQuestion() {
         countdown = setTimeout(check, 5000);
     }
     else {
-        $("main").html("<div>You got " + correct + " out of " + questions.length + " correct!");
+        $("#timer").empty();
+        $("#questions").empty();
+        $("#results").html("<div>You got " + correct + " out of " + questions.length + " correct!</div> <div id='restart'>Click this message to start again</div>");
     }
 }
 
@@ -78,3 +86,4 @@ setInterval(function () {
 
 start();
 $("body").on("click", ".answer", check);
+$("body").on("click", "#restart", start);
